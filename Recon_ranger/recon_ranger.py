2236866719228234edaa76f00fc744a603f1aa76
pavankumar143-coder <117728691+pavankumar143-coder@users.noreply.github.com>
2024-06-26 02:20:05 +0530

    Update recon_ranger.py

--- Recon_ranger/recon_ranger.py
+++ Recon_ranger/recon_ranger.py
@@ -43,32 +43,36 @@ class ReconRanger:
     def live_subdomains(self):
         main_title = "Live Subdomains"
         print("Identifying Live Subdomains...")
-        subprocess.run(f"httpx -l -o {self.output_dir}/live_subdomains.txt {self.output_dir}/subdomains.txt", shell=True)
+        #subprocess.run(f"httpx -l -o {self.output_dir}/live_subdomains.txt {self.output_dir}/subdomains.txt", shell=True)
+        subprocess.run(f"cat {self.output_dir}/subdomains.txt | httpx > {self.output_dir}/live_subdomains.txt ", shell=True)
 
     #Subdomain Takeovers
     def subdomain_takeover(self):
         main_title = "Subdomain Takeovers"
         print("Checking for Subdomain Takeovers...")
-        subprocess.run(f"subzy -d {self.target} -o {self.output_dir}/subdomain_takeover.txt", shell=True)
+        #subprocess.run(f"subzy -d {self.target} -o {self.output_dir}/subdomain_takeover.txt", shell=True)
+        subprocess.run(f"subzy run --targets {self.output_dir}/live_subdomains.txt   --output {self.output_dir}/subdomain_takeover.txt"   , shell=True)
 
     #Finding Open Ports
     def open_ports(self):
         main_title = "Finding Open Ports"
         print("Finding Open Ports...")
         subprocess.run(f"naabu -host {self.target} -o {self.output_dir}/open_ports.txt", shell=True)
-
+        
     #JSON Files Finding
     def json_files_discovery(self):
         main_title = "Searching JSON"
         print("Searching for JSON Files...")
-        subprocess.run(f"waybackurls {self.target} -o {self.output_dir}/json_files.txt", shell=True)
+        #subprocess.run(f"waybackurls {self.target} -o {self.output_dir}/json_files.txt", shell=True)
+        subprocess.run(f"cat {self.output_dir}/live_subdomains.txt  | waybackurls > {self.output_dir}/json_files.txt", shell=True)
+
 
     #Javascript Finding
     def js_vulnerabilities(self):
         main_title = "Finding Js Files"
         print("Identifying JavaScript Vulnerabilities...")
         subprocess.run(f"katana -u {self.target} -o {self.output_dir}/js_vulnerabilities.txt", shell=True)
-        subprocess.run(f"subjs -u {self.target} -o {self.output_dir}/subjs.txt", shell=True)
+        subprocess.run(f"subjs -i  {self.output_dir}/live_subdomains.txt  -o {self.output_dir}/subjs.txt", shell=True)
         subprocess.run(f"httpx -u {self.target} -o {self.output_dir}/httpx_js.txt", shell=True)
         subprocess.run(f"nuclei -u {self.target} -o {self.output_dir}/nuclei_js.txt", shell=True)
 
@@ -98,7 +102,7 @@ class ReconRanger:
     def open_redirection(self):
         main_title = "find open Redirect"
         print("Identifying Open Redirections...")
-        subprocess.run(f"uro -u {self.target} -o {self.output_dir}/open_redirection.txt", shell=True)
+        subprocess.run(f"uro -i  {self.output_dir}/live_subdomains.txt -o {self.output_dir}/open_redirection.txt", shell=True)
     
     #Finding Comprehensive
     def comprehensive_vulnerability_scanning(self):
